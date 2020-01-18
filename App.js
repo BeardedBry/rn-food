826d795173318d5342c835f323fc62c27465b54d
Brian Burkard <brianburkard@gmail.com>
2020-01-18 11:52:20 -0500

    react navigation and stack

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import {Text} from 'react-native';
+import SearchScreen from './src/screens/SearchScreen';
+
+const navigator = createStackNavigator({
+  Search: SearchScreen
+}, {
+  initialRouteName: 'Search',
+  defaultNavigationOptions: {
+    title: 'BusinessSearch'
+  }
+});
+
 
 const App = () => {
-  return <Text>Hello World!</Text>
+  return <Text>Hello World! test</Text>
 }
 
-export default App;
+export default createAppContainer(navigator);
